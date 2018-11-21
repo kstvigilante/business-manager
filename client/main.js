5e7ef46d74b9f5513af51a2383a741f1b07f16c8
kstvigilante <kst85077@gmail.com>
2018-11-21 12:31:39 +0530

    added footer

--- client/main.js
+++ client/main.js
@@ -2,3 +2,4 @@ import "./main.html";
 import "../imports/templates/header/header.js";
 import "../imports/pages/business-manager1/business-manager1.js";
 import "../imports/pages/business/business.js";
+import "../imports/templates/footer/footer.js";
